ebef8704b8d73e1e2a25374257f8f7d9532b2f35
devpola <68686204+devpola@users.noreply.github.com>
2021-01-21 22:11:46 +0900

    blazor TodoList 예제 실습

--- TodoList/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ TodoList/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace TodoList.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 1 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 2 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 3 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 4 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 5 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 6 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 7 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 8 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 9 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using TodoList;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\_Imports.razor"
+#line 10 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/_Imports.razor"
 using TodoList.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using TodoList.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 33 "C:\Users\admin\Documents\GitHub\Blazor\TodoList\Shared\NavMenu.razor"
+#line 33 "/Users/kimminsu/Documents/GitHub/Blazor/TodoList/Shared/NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
