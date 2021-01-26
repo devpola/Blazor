fb8bcab59dac779d4e735c5b05ea6ea02cb5da68
devpola <68686204+devpola@users.noreply.github.com>
2021-01-26 19:58:20 +0900

    blazor razor 페이지에서 모듈 사용 테스트

---
+++ ModuleTest/Data/Communication.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ModuleTest.Data
+{
+    public class Communication
+    {
+        public static void Hello2()
+        {
+            Console.WriteLine("Hello2");
+        }
+    }
+}
---
+++ ModuleTest/Data/Test.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ModuleTest.Data
+{
+    public class Test
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
+            Communication.Hello2();
+        }
+    }
+}
